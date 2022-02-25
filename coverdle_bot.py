849c5c0bf76d98442e4caa8fe04627618cdff27b
Matthew Cover <matthewncover@outlook.com>
2022-02-25 11:11:32 -0700

    first commit

---
+++ coverdle_bot.py
@@ -0,0 +1,21 @@
+import discord, pandas as pd, re, json
+
+client = discord.Client()
+guild = discord.Guild
+
+pass
+
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+
+#         return NotImplementedError
+
+#     elif re.search(r'(Wordle) \d+ \d{1}[/]6', message.content):
+
+#         # append data
+#         # variables:
+#             # 
+#         pass
+
+
