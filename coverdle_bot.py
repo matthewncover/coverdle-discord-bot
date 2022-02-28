13b9e57f55b3723c3bd2634ef4e3190d432181cf
Matthew Cover <matthewncover@outlook.com>
2022-02-27 19:04:08 -0700

    command and cmd args

--- coverdle_bot.py
+++ coverdle_bot.py
@@ -1,10 +1,57 @@
 import discord
+from discord.ext import commands
 
-import pandas as pd, re, json, numpy as np, datetime as dt
+import pandas as pd, re, json, numpy as np
+
+import datetime as dt
+from dateutil.relativedelta import relativedelta
 
 id_dict = json.load(open("ids.json"))
 
-class coverdle_client(discord.Client):
+class CoverdleClient(discord.Client):
+
+    # def __init__(self):
+
+    #     # self.bot_cmd = commands.Bot(command_prefix = '$420')
+    #     self.ass_eating = "all day"
+
+    def on_command(self, msg):
+
+        # self.bot_cmd = commands.Bot(command_prefix="$420")
+
+        # @commands.command()
+        # async def request_history(context, *args):
+
+        #     await context.send(f'{len(args)}')
+
+        # self.bot_cmd.add_command(request_history)
+        
+
+        self.cmd_args = msg.content.split(' ')[1:]
+        self.cmd_arg_primary = self.cmd_args[0]
+
+        self.report = Reporting(self.cmd_args)
+
+        # if self.cmd_arg_primary == 'history':
+            
+            # self.report = Reporting(self.cmd_args)
+            # pass
+
+    def on_wordle_entry(self, msg):
+
+        self.author = f"{msg.author.name}#{msg.author.discriminator}"
+
+        try:
+            self.author_name = id_dict['author-ids'][self.author]
+        except:
+            self.author_name = "Who?"
+        
+        self.msg_day = msg.created_at.strftime("%Y%m%d")
+        self.game = self.rdle_names[self.rdle_bools.index(True)]
+
+        self.read_coverdle_data()
+        self.find_score(msg)
+        self.append_data()
 
     def read_coverdle_data(self):
 
@@ -12,6 +59,8 @@ class coverdle_client(discord.Client):
 
         # self.df.date = pd.to_datetime(self.df.date)
         # self.df.time = pd.to_datetime(self.df.time)
+
+        pass
 
     def which_game(self, msg):
 
@@ -24,13 +73,14 @@ class coverdle_client(discord.Client):
 
     def find_score(self, msg):
 
+        self.is_hard_mode = 0
+
         if self.game != "Quordle":
 
             self.score = re.search(r'(\d{1}|X)[/]6', msg.content).group()[0]
-            self.score = 0 if self.score == "X" else int(self.score)
+            self.score = "X" if self.score == "X" else int(self.score)
             self.quordle_scores = [np.nan]*4
 
-            self.is_hard_mode = 0
             if self.game == 'Wordle':
                 self.is_hard_mode = int(
                     msg.content[
@@ -39,8 +89,6 @@ class coverdle_client(discord.Client):
                         )
             
         else:
-
-            self.score = np.nan
 
             quordle_message_score_contents = (
                 msg.content[
@@ -52,13 +100,15 @@ class coverdle_client(discord.Client):
 
             self.quordle_scores = (
                 [
-                    0 if z == '\U0001f7e5' else int(z)
+                    "X" if z == '\U0001f7e5' else int(z)
                     for z in [
                         x for x in list(quordle_message_score_contents)
                         if x not in ('️', '⃣')
                         ]
                 ]
             )
+
+            self.score = np.array(self.quordle_scores).mean()*(2/3) # convert to max score of 6 scale
 
 
     def append_data(self):
@@ -78,8 +128,15 @@ class coverdle_client(discord.Client):
 
             self.df.to_csv("./coverdle_data.csv", index=None)
 
-
     async def on_message(self, msg):
+
+        #####################################
+        # first, checks db and updates with history
+
+        for m in msg.channel.history():
+            print(m)
+
+        #####################################
 
         if msg.author == client.user:
             return None
@@ -87,55 +144,108 @@ class coverdle_client(discord.Client):
         if msg.channel.name != 'wordle':
             return None
 
-        self.which_game(msg)
-        rdle_names = ["Wordle", "Worldle", "Nerdle", "Quordle"]
+        if msg.content.startswith("$420"):
+            self.on_command(msg)
 
-        if max(self.rdle_bools) == 0:
-            # check if command
+        self.which_game(msg)
+        self.rdle_names = ["Wordle", "Worldle", "Nerdle", "Quordle"]
+
+        if max(self.rdle_bools) == 1:
+            self.on_wordle_entry(msg)
+
+        # regular chat
+        else:
             return None
         
-        self.author = f"{msg.author.name}#{msg.author.discriminator}"
-
-        try:
-            self.author_name = id_dict['author-ids'][self.author]
-        except:
-            self.author_name = "Who?"
         
-        self.msg_day = msg.created_at.strftime("%Y%m%d")
-        self.game = rdle_names[self.rdle_bools.index(True)]
 
-        self.read_coverdle_data()
-        self.find_score(msg)
-        self.append_data()
+class Reporting:
 
-class reporting:
+    def __init__(self, cmd_args:list):
 
-    def __init__(self):
+        self.df = CoverdleClient.read_coverdle_data()
+        self.df.date = pd.to_datetime(self.df.date)
 
-        self.df = coverdle_client.read_coverdle_data()
         self.report_msg = ''
+
+        self.game_filter = cmd_args[0]
+        self.stats_filter = cmd_args[1]
+        self.date_filter = dt.datetime.strptime(cmd_args[2], "%b%y")
+        self.define_fn_dicts()
+        
+    def define_fn_dicts(self):
+
+        # self.game_filter_fn_dict = {
+        #     "all_games": 
+        # }
+
+        self.stats_fn_dict = {
+            "all_stats": self.all_stats,
+            "user_performance": self.user_performance,
+            "game_popularity": self.game_popularity,
+            "team_performance": self.team_performance,
+        }
+        self.date_filter_fn_dict = {
+            "all_time": self.unspecified_date,
+            self.date_filter: self.specified_month
+        }
+
+
+    def specified_game(self):
+
+        games = (
+            self.df.game.unique().tolist()
+            if self.game_filter == 'all_games'
+            else [self.game_filter]
+        )
+
+        self.df = (
+            self.df[
+                self.df.game.isin(games)
+            ]
+        )
+
+    def specified_month(self):
+
+        # another input: table that results from filtering/melting/whatever based on secondary arg
+
+        # self.report_msg += f'{monthyear}\n--------\n'
+
+        one_month_before = self.date_filter - relativedelta(months=1)
+
+        self.df = (
+            self.df[
+                (self.df.date >= self.date_filter) &
+                (self.df.date < one_month_before)
+            ]
+        )
+
+    def unspecified_date(self):
+
+        pass
 
     def user_performance(self):
 
         pass
 
-    def num_games_played(self):
+    def game_popularity(self):
 
         pass
 
-    def aggregate_over_month(self, monthyear):
-
-        self.report_msg += f'{monthyear}\n--------\n'
+    def team_performance(self):
 
         pass
 
-    def aggregate_all_time(self):
+    def all_stats(self):
+
+        pass
+
+    def compile_report(self):
 
         pass
 
 
-
-client = coverdle_client()
+client = CoverdleClient()
 guild = discord.Guild
 channel = client.get_channel(id_dict['channel-id'])
 
